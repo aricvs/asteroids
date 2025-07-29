944ba93387c1ec346c21535f0e2b525ca7a81e36
Ariel <146785399+aricvs@users.noreply.github.com>
2025-07-29 19:05:23 -0300

    Implement rotation via A and D keys

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         new_player.draw(screen)
+        new_player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
